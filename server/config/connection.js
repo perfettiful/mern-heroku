4cb4b1115c158fbcd8418a784b785ae442ffc057
Nathan Perfetti <nathanperfetti@gmail.com>
2023-02-03 20:19:00 -0600

    update node version

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-console.log("Connecting to MongoDB...", process.env.MONGODB_URI )
+console.log("Connecting to MongoDB...", process.env )
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
   {
