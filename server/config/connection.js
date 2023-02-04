194c0627b7861a3767f318e2de5056d2e3e98477
Nathan Perfetti <nathanperfetti@gmail.com>
2023-02-03 19:50:32 -0600

    inital setup for deploy

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log("Connecting to MongoDB...", process.env.MONGODB_URI )
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
